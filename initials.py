c4d124c0e422ad38fbf2db6b4d6e147297aa3808
j-palacio <jaypalacio95.com>
2023-12-23 07:20:47 -0500

    Add initials.py

---
+++ initials.py
@@ -0,0 +1,13 @@
+# Twitter: @jpalacio305
+# Fun Fact: I have a ptibull named Neo.
+
+name = """
+    JJJJJJJJ  PPPPPPPPP
+        J     P       P
+        J     P       P
+        J     PPPPPPPP
+  J     J     P 
+  J     J     P
+    J J       P          """
+    
+print(name)
